18e8598e093eaa09942a0403d5db73845a2d1bf2
bryce <brycelang@me.com>
2018-07-26 19:17:48 -0700

    Trivia
    Added radio buttons and html framework for the quiz
    
    checks to see if ur answer is correct and alerts you

--- javascript/game.js
+++ javascript/game.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { 
 var question = document.getElementById('question');
 var answers = document.getElementById('answers');
-var submitButton = document.getElementById('submit');
+var submitButton = document.getElementById('#submit');
 
 function init(){}
 
@@ -10,38 +10,22 @@ function conclude(){}
 //init the game when the webpage is loaded
 init();
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
+
+
+$("button").on("click", function() {
+    var amountCorrect = 0;
+for(var i = 0; i <= 45; i++) {
+  var radios = document.getElementsByName("group" + i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value === "correct" && radio.checked) {
+      amountCorrect++;
+      alert("Correct Responses: " + amountCorrect);
+    }
+  }
+}
+});
 
 //https://trivia.fyi/
-var questions = [
-    {
-      question: "The southernmost part of the US is located in which state?",
-      answers: {
-        a: "Florida",
-        b: "New York",
-        c: "Hawaii"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "What was the first console video game that allowed the game to be saved?",
-      answers: {
-        a: "Mario",
-        b: "Pac-Man",
-        c: "The Legend of Zelda"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Created in 2009, what was the first decentralized cryptocurrency?",
-      answers: {
-        a: "Bitcoin",
-        b: "Dogecoin",
-        c: "Litecoin"
-      },
-      correctAnswer: "a"
-    }
-  ];
 
 });
